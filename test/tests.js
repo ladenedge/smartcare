6e0db4cdf0a99cbce23484641ae4fd9a02936779
Jay Miller <jnmiller@cryptofreak.org>
2017-04-25 15:25:56 -0600

    Broke out utility methods to lib.

--- test/tests.js
+++ test/tests.js
@@ -2,7 +2,7 @@
 var assert = require('assert');
 var sinon = require('sinon');
 var request = require('request');
-var T3Auth = require('../t3services');
+var T3Service = require('../t3services');
 
 var validConfig = {
     endpoint: 'https://t3.sc.com',
@@ -19,49 +19,49 @@ var requiredParams = ['endpoint', 'customer', 'app', 'secret'];
 
 describe('constructor', function() {
     it('should throw on undefined config', function() {
-        assert.throws(() => new T3Auth(), Error);
+        assert.throws(() => new T3Service(), Error);
     });
     it('should throw on null config', function() {
-        assert.throws(() => new T3Auth(null), Error);
+        assert.throws(() => new T3Service(null), Error);
     });
     Object.keys(validConfig).forEach(prop => {
         it(`should throw on mistyped property ${prop}`, function() {
             let invalidConfig = Object.assign({}, validConfig);
             invalidConfig[prop] = function() { };
-            assert.throws(() => new T3Auth(invalidConfig), TypeError);
+            assert.throws(() => new T3Service(invalidConfig), TypeError);
         });
     });
     requiredParams.forEach(prop => {
         it(`should throw on undefined property ${prop}`, function() {
             let invalidConfig = Object.assign({}, validConfig);
             delete invalidConfig[prop];
-            assert.throws(() => new T3Auth(invalidConfig), Error);
+            assert.throws(() => new T3Service(invalidConfig), Error);
         });
         it(`should throw on null property ${prop}`, function() {
             let invalidConfig = Object.assign({}, validConfig);
             invalidConfig[prop] = null;
-            assert.throws(() => new T3Auth(invalidConfig), Error);
+            assert.throws(() => new T3Service(invalidConfig), Error);
         });
     });
     requiredParams.filter(v => typeof validConfig[v] === 'string').forEach(prop => {
         it(`should throw on empty string property ${prop}`, function() {
             let invalidConfig = Object.assign({}, validConfig);
             invalidConfig[prop] = "";
-            assert.throws(() => new T3Auth(invalidConfig), Error);
+            assert.throws(() => new T3Service(invalidConfig), Error);
         });
         it(`should throw on whitespace string property ${prop}`, function() {
             let invalidConfig = Object.assign({}, validConfig);
             invalidConfig[prop] = " \t ";
-            assert.throws(() => new T3Auth(invalidConfig), Error);
+            assert.throws(() => new T3Service(invalidConfig), Error);
         });
     });
     it('should not throw on valid config', function() {
-        assert.doesNotThrow(() => new T3Auth(validConfig));
+        assert.doesNotThrow(() => new T3Service(validConfig));
     });
 });
 
 describe('login()', function() {
-    var t3auth = new T3Auth(validConfig);
+    var t3svc = new T3Service(validConfig);
     var validHandlers = {
         onSuccess: function(rsp) { },
         onError: function(err) { }
@@ -82,25 +82,25 @@ describe('login()', function() {
 
     badStringValues.forEach(arg => {
         it(`should throw when username is '${arg}'`, function() {
-            assert.throws(() => t3auth.login(arg, "pw", validHandlers), Error);
+            assert.throws(() => t3svc.login(arg, "pw", validHandlers), Error);
         });
     });
     badStringValues.forEach(arg => {
         it(`should throw when password is '${arg}'`, function() {
-            assert.throws(() => t3auth.login("un", arg, validHandlers), Error);
+            assert.throws(() => t3svc.login("un", arg, validHandlers), Error);
         });
     });
     it(`should throw when handlers are undefined`, function() {
-        assert.throws(() => t3auth.login("un", "pw"), Error);
+        assert.throws(() => t3svc.login("un", "pw"), Error);
     });
     it(`should throw when handlers are null`, function() {
-        assert.throws(() => t3auth.login("un", "pw", null), Error);
+        assert.throws(() => t3svc.login("un", "pw", null), Error);
     });
     Object.keys(validHandlers).forEach(hand => {
         it(`should throw when ${hand} handler is missing`, function() {
             let invalidHandlers = Object.assign({}, validHandlers);
             delete invalidHandlers[hand];
-            assert.throws(() => t3auth.login("un", "pw", invalidHandlers), Error);
+            assert.throws(() => t3svc.login("un", "pw", invalidHandlers), Error);
         });
     });
     Object.keys(validHandlers).forEach(hand => {
@@ -108,7 +108,7 @@ describe('login()', function() {
             it(`should throw when ${hand} handler is '${val}'`, function() {
                 let invalidHandlers = Object.assign({}, validHandlers);
                 invalidHandlers[hand] = val;
-                assert.throws(() => t3auth.login("un", "pw", invalidHandlers), Error);
+                assert.throws(() => t3svc.login("un", "pw", invalidHandlers), Error);
             });
         });
     });
@@ -117,7 +117,7 @@ describe('login()', function() {
             { key: 'url', val: validConfig.endpoint },
         ].forEach(opt => {
             it(`should get configured ${opt.key}`, function() {
-                t3auth.login("un", "pw", validHandlers);
+                t3svc.login("un", "pw", validHandlers);
                 assert.equal(this.post.firstCall.args[0][opt.key], opt.val);
             });
         });
@@ -128,17 +128,17 @@ describe('login()', function() {
             { name: 'Accept', val: 'application/json' },
         ].forEach(opt => {
             it(`should get HTTP header ${opt.name}`, function() {
-                t3auth.login("un", "pw", validHandlers);
+                t3svc.login("un", "pw", validHandlers);
                 assert.equal(this.post.firstCall.args[0]['headers'][opt.name], opt.val);
             });
         });
         it(`should get GUID in HTTP header X-SpeechCycle-SmartCare-SessionID`, function() {
-            t3auth.login("un", "pw", validHandlers);
+            t3svc.login("un", "pw", validHandlers);
             assert(this.post.firstCall.args[0]['headers']['X-SpeechCycle-SmartCare-SessionID'].match(/[0-9A-F]{8}-?([0-9A-F]{4}-?){3}-?[0-9A-F]{12}/i));
         });
         Object.keys(validBody).forEach(key => {
             it(`should put ${key} property in body`, function() {
-                t3auth.login("un", "pw", validHandlers);
+                t3svc.login("un", "pw", validHandlers);
                 assert.equal(this.post.firstCall.args[0]['json'][key], validBody[key]);
             });
         });
@@ -152,7 +152,7 @@ describe('login()', function() {
                 assert.equal(err.message, 'aaa');
                 done();
             };
-            t3auth.login("un", "pw", validHandlers);
+            t3svc.login("un", "pw", validHandlers);
         });
         it(`should call onError on missing WWW-Authenticate`, function(done) {
             var rsp = { headers: { "Other-Header": 'value' } }
@@ -162,7 +162,7 @@ describe('login()', function() {
                 assert(err.message.startsWith('Challenge'));
                 done();
             };
-            t3auth.login("un", "pw", validHandlers);
+            t3svc.login("un", "pw", validHandlers);
         });
         it(`should call onError on wrong WWW-Authenticate type`, function(done) {
             var rsp = { headers: { "WWW-Authenticate": 'Basic realm="sc.com"' } }
@@ -172,7 +172,7 @@ describe('login()', function() {
                 assert(err.message.startsWith('Challenge'));
                 done();
             };
-            t3auth.login("un", "pw", validHandlers);
+            t3svc.login("un", "pw", validHandlers);
         });
     });
     describe('final request', function() {
@@ -191,14 +191,14 @@ describe('login()', function() {
         ].forEach(hdr => {
             it(`should POST with same ${hdr} header`, function(done) {
                 validHandlers.onError = err => { done(); };
-                t3auth.login("un", "pw", validHandlers);
+                t3svc.login("un", "pw", validHandlers);
 
                 assert.equal(this.post.secondCall.args[0]['headers'][hdr], this.post.firstCall.args[0]['headers'][hdr]);
             });
         });
         it(`should POST with Authorization header`, function(done) {
             var config = Object.assign({ sessionId: '15344b6f-2131-2fa9-994e-c69103be9859' }, validConfig);
-            var t3client = new T3Auth(config);
+            var t3client = new T3Service(config);
             validHandlers.onError = err => { done(); };
             t3client.login("un", "pw", validHandlers);
 
@@ -214,7 +214,7 @@ describe('login()', function() {
                 this.post.callsArgWith(1, null, rsp);
 
                 validHandlers.onError = err => { done(); };
-                t3auth.login("un", "pw", validHandlers);
+                t3svc.login("un", "pw", validHandlers);
 
                 assert(this.post.secondCall.args[0]['headers']['Authorization'].startsWith(rsp.headers.hdr));
             });
@@ -222,7 +222,7 @@ describe('login()', function() {
         Object.keys(validBody).forEach(key => {
             it(`should put ${key} property in body`, function(done) {
                 validHandlers.onError = err => { done(); };
-                t3auth.login("un", "pw", validHandlers);
+                t3svc.login("un", "pw", validHandlers);
 
                 assert.equal(this.post.secondCall.args[0]['json'][key], validBody[key]);
             });
@@ -243,7 +243,7 @@ describe('login()', function() {
                 assert.equal(err.message, 'aaa');
                 done();
             };
-            t3auth.login("un", "pw", validHandlers);
+            t3svc.login("un", "pw", validHandlers);
         });
         it(`should call onError on non-200 status code`, function(done) {
             var rsp = { statusCode: 400 }
@@ -253,7 +253,7 @@ describe('login()', function() {
                 assert.equal(err.message, 'Authentication failed');
                 done();
             };
-            t3auth.login("un", "pw", validHandlers);
+            t3svc.login("un", "pw", validHandlers);
         });
         it(`should call onSuccess on with message body`, function(done) {
             var rsp = { statusCode: 200 };
@@ -264,12 +264,12 @@ describe('login()', function() {
                 assert.equal(rsp.test, 'aaa');
                 done();
             };
-            t3auth.login("un", "pw", validHandlers);
+            t3svc.login("un", "pw", validHandlers);
         });
         it(`should call onSuccess in verbose mode`, function(done) {
             var config = Object.assign({}, validConfig);
             config.verbose = true;
-            var t3client = new T3Auth(config);
+            var t3client = new T3Service(config);
             var rsp = { statusCode: 200 };
             var body = { test: 'aaa' };
             this.post.onSecondCall().callsArgWith(1, null, rsp, body);
