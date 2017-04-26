bf65d15393a2ede5014699b6fb0cac9ce7211315
Jay Miller <jnmiller@cryptofreak.org>
2017-04-26 14:35:07 -0600

    Added search() function.

--- t3services.js
+++ t3services.js
@@ -1,19 +1,60 @@
 'use strict'
 
 var request = require('request');
-var guid = require('uuid');
-var tokenizer = require('./lib/auth-token');
+var t3util = require('./lib/t3util');
 var insensitiveGet = require('./lib/insensitive-get');
 var validator = require('./lib/validate');
 
 var configSchema = [
-    { key: 'endpoint', type: 'string', req: true },
+    { key: 'endpoints', type: 'Array', req: true },
     { key: 'proxy', type: 'string', req: false },
     { key: 'customer', type: 'string', req: true },
     { key: 'app', type: 'string', req: true },
     { key: 'secret', type: 'string', req: true },
+    { key: 'agent', type: 'string', req: false },
     { key: 'verbose', type: 'boolean', req: false }
 ];
+
+var endpointsSchema = [
+    { key: 'login', type: 'string', req: true },
+    { key: 'search', type: 'string', req: true },
+    { key: 'account', type: 'string', req: true },
+];
+
+/**
+ * Result of a successful login command.
+ * @typedef {Object} AuthToken
+ * @property {string} Value The authenticated user's username.
+ * @property {Array} AdditionalValues An array of arbitrary key-value pairs.
+ * @property {string} FirstName The user's first name, if available.
+ * @property {string} LastName The user's last name, if available.
+ * @property {string} Phone The user's phone number, if available.
+ * @property {string} Created The time at which this token was created.
+ * @property {string} UserData An identifier for this user (eg. an email address).
+ * @property {string} T3Token The authentication token to use for other T3 services.
+ */
+
+/**
+ * Result of a successful search command.
+ * @typedef {Object} Action
+ * @property {string} Action Name of the action.
+ * @property {string} Query Unused.
+ * @property {number} Confidence Confidence value that this action is relevant to the user.
+ * @property {string} Text Original text of the user's query.
+ * @property {string} ConfirmationText Confirmation text to show the user.
+ */
+
+/**
+ * Function called on successful authentication attempts.
+ * @callback onSuccess  
+ * @param {AuthToken|Action} response An object created from the body of the successful response.
+ */
+
+/**
+ * Function called on failed authentication attempts.
+ * @callback onError
+ * @param {Object} error An Error object containing information about the failure.
+ */
 
 /**
  * Authentication module for the T3 speech services.
@@ -31,26 +72,8 @@ class T3Service {
      */
     constructor(config) {
         this.t3config = validator.validateConfig(config, configSchema);
+        this.t3config.endpoints = validator.validateConfig(config.endpoints, endpointsSchema);
     }
-
-    /**
-     * Function called on successful authentication attempts.
-     * @callback onSuccess
-     * @param {Object} response An object created from the body of the successful response.
-     * @param {string} response.Value The authenticated user's username.
-     * @param {Array} response.AdditionalValues An array of arbitrary key-value pairs.
-     * @param {string} response.FirstName The user's first name, if available.
-     * @param {string} response.LastName The user's last name, if available.
-     * @param {string} response.Phone The user's phone number, if available.
-     * @param {string} response.Created The time at which this token was created.
-     * @param {string} response.UserData An identifier for this user (eg. an email address).
-     * @param {string} response.T3Token The authentication token to use for other T3 services.
-     */
-    /**
-     * Function called on failed authentication attempts.
-     * @callback onError
-     * @param {Object} error An Error object containing information about the failure.
-     */
 
     /**
      * Attempts the authentication procedure.
@@ -65,22 +88,12 @@ class T3Service {
         password = validator.validateString(password, 'password');
         validator.validateResponseHandlers(responseHandlers);
 
-        var sessionId = this.t3config.sessionId || guid();
-        var opts = {
-            url: this.t3config.endpoint,
-            proxy: this.t3config.proxy,
-            headers: {
-                'X-SpeechCycle-SmartCare-CustomerID': this.t3config.customer,
-                'X-SpeechCycle-SmartCare-ApplicationID': this.t3config.app,
-                'X-SpeechCycle-SmartCare-SessionID': sessionId,
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            },
-            json: {
-                'ID': username,
-                'Password': password,
-                'AdditionalValuesVersion': '2'
-            }
+        var opts = t3util.requestOptions(this.t3config);
+        opts.url = this.t3config.endpoints.login;
+        opts.json = {
+            'ID': username,
+            'Password': password,
+            'AdditionalValuesVersion': '2'
         };
 
         request.debug = !!this.t3config.verbose;
@@ -91,7 +104,7 @@ class T3Service {
             if (!authHeader || !authHeader.match(/^T3Auth /i))
                 responseHandlers.onError(new Error('Challenge not found'));
 
-            var token = tokenizer(this.t3config, sessionId);
+            var token = t3util.createToken(this.t3config, opts.headers['X-SpeechCycle-SmartCare-SessionID']);
             opts.headers['Authorization'] = authHeader + `, token=${token}`;
 
             request.post(opts, (error, rsp, body) => {
@@ -118,7 +131,33 @@ class T3Service {
         return this.auth && this.auth.T3Token && this.auth.T3Token.length > 0;
     }
 
-    search(query) {
+    /**
+     * Searches for actions related to a query by a user.
+     * @param {string} query User's search query.
+     * @param {Object} responseHandlers An object that contains callbacks for search results.
+     * @param {onSuccess} responseHandlers.onSuccess Function to call if the search is successful.
+     * @param {onError} responseHandlers.onError Function to call in case of error.
+     */
+    search(query, responseHandlers) {
+        query = validator.validateString(query, 'query');
+        validator.validateResponseHandlers(responseHandlers);
+
+        var opts = t3util.requestOptions(this.t3config);
+        opts.url = this.t3config.endpoints.search + '/simple';
+        opts.qs = { text: query };
+
+        request.debug = !!this.t3config.verbose;
+        request.post(opts, (error, rsp, body) => {
+            if (error)
+                responseHandlers.onError(error);
+            if (rsp.statusCode !== 200)
+                responseHandlers.onError(new Error('Search failed'));
+
+            if (this.t3config.verbose)
+                console.error(body);
+
+            responseHandlers.onSuccess(body);
+        });
     }
 };
 
